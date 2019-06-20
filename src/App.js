ea2f460f1517303daa64f0a1afb82b79e948d55d
Hilmi Abou-Saleh <2ndgenairborne@gmail.com>
2019-06-20 13:10:20 -0500

    Add commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
     super();
     this.state = {
       response: false,
-      endpoint: "http://0.0.0.0:4001"
+      endpoint: "http://localhost:4001"
     };
   }
   componentDidMount() {
