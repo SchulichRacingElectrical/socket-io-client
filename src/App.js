162c740b59e4671c6fe13cb9b19083ce2f77c3a4
Hilmi Abou-Saleh <2ndgenairborne@gmail.com>
2019-06-20 13:25:06 -0500

    Add commit

--- src/App.js
+++ src/App.js
@@ -3,16 +3,36 @@ import socketIOClient from "socket.io-client";
 class App extends Component {
   constructor() {
     super();
+    this.streamDash = React.createRef();
+    this.socket = "";
     this.state = {
-      response: false,
+      data: {},
       endpoint: "http://schulichracing.com:4001"
     };
   }
   componentDidMount() {
+    
     const { endpoint } = this.state;
-    const socket = socketIOClient(endpoint);
-    socket.on("FromAPI", data => this.setState({ response: data }));
+    this.socket = socketIOClient(endpoint);
   }
+
+
+  componentDidMount() {    // Fetch the list on first mount
+    this.timerID = setInterval(() => this.tick(), 100);
+  }
+
+  tick() {
+    this.pullData();
+    this.forceUpdate(); 
+  }
+
+  pullData() {
+    socket.on("FromAPI", redis_data => this.setState({ data: redis_data.json() }));
+    this.streamDash.current.insertData(this.state.data);
+    //this.graphElement.current.pushData(this.state.data);
+  }
+
+
   render() {
     const { response } = this.state;
     return (
